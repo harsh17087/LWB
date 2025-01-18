5fc6e004cde0bb6d242a3a32d8838d43ffbfdda7
Harsh raj <harshraj17087@gmail.com>
2025-01-18 19:06:02 +0530

    added intro section

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,88 @@
-import Image from 'next/image';
-import LWBLogo from '@/assets/images/LWB_Logo.jpg';
+"use client";
+import LWBLogo from "@/assets/images/LWB_Logo.jpg";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { Menu, MenuItem } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 function Header() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   return (
-    <header className="flex flex-col sm:flex-row justify-between items-center p-4 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 text-white">
-      <div className="flex items-center mb-4 sm:mb-0">
-        <Image src={LWBLogo} alt="Learning with basics Logo" width={40} height={40} className="rounded-full" />
-        <span className="ml-2 text-xl font-bold">Learning With Basics</span>
+    <header className="flex sm:flex-row justify-between items-start p-1 bg-gradient-to-r from-[#a9b2ff] via-[#ffa9f9] to-[#a9b2ff] h-24">
+      <div className="flex items-center sm:mb-0 space-x-4 m-2">
+        <Image
+          src={LWBLogo}
+          alt="Learning with basics Logo"
+          width={30}
+          height={30}
+          className="rounded-full"
+        />
+        <span className="font-bold text-xl">Learning with Basics</span>
       </div>
-      <div className="flex space-x-4">
-        <button className="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded">
-          Login / Register
-        </button>
+      <div className="flex items-center m-2">
+        <div className="hidden sm:flex items-center">
+          <Link
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group flex items-center justify-center h-full w-12 border-r border-gray-600"
+          >
+            <FacebookOutlinedIcon
+              sx={{ fontSize: 30, color: "white" }}
+              className="group-hover:text-blue-600"
+            />
+          </Link>
+          <Link
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group flex items-center justify-center h-full w-12 border-l border-r border-gray-600"
+          >
+            <InstagramIcon
+              sx={{ fontSize: 30, color: "white" }}
+              className="group-hover:text-orange-600"
+            />
+          </Link>
+          <Link
+            href="https://www.youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group flex items-center justify-center h-full w-12 border-l border-r border-gray-600"
+          >
+            <YouTubeIcon
+              sx={{ fontSize: 35, color: "white" }}
+              className="group-hover:text-red-600"
+            />
+          </Link>
+        </div>
+        <div className="relative flex items-center">
+          <AccountCircleOutlinedIcon
+            sx={{ fontSize: 30, color: "white" }}
+            className="cursor-pointer ml-1 hover:text-black sm:hidden"
+            onClick={(e) => setAnchorEl(e.target as HTMLElement)}
+          />
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={() => setAnchorEl(null)}
+          >
+            <MenuItem onClick={() => setAnchorEl(null)}>
+              <Link href="/login">Login</Link>
+            </MenuItem>
+            <MenuItem onClick={() => setAnchorEl(null)}>
+              <Link href="/register">Register</Link>
+            </MenuItem>
+          </Menu>
+          <div className="hidden sm:flex space-x-4 ml-2">
+            <Link href="/login" className="text-white hover:text-black">
+              Login / Register
+            </Link>
+          </div>
+        </div>
       </div>
     </header>
   );
--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,28 +1,87 @@
-import React from 'react';
-import Image from 'next/image';
-import topImage from '@/assets/images/LWB_Top_Image.jpg';
-import SKJImage from '@/assets/images/LWB_SKJ.png';
-import './Introduction.css';
+import React from "react";
+import Image from "next/image";
+import SKJImage from "@/assets/images/LWB_SKJ.png";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import "./Introduction.css";
+import SchoolIcon from "@mui/icons-material/School";
+import PeopleIcon from "@mui/icons-material/People";
+const InfoBadge: React.FC = () => {
+  return (
+    <div className="info-badge inline-flex items-center bg-white p-2 rounded-lg shadow-md animate-float">
+      <SchoolIcon className="text-blue-500" />
+      <div className="ml-2">
+        <p className="text-sm font-bold">200+ Courses</p>
+        <p className="text-xs">Courses & Video</p>
+      </div>
+    </div>
+  );
+};
+
+const InfoBadge2: React.FC = () => {
+  return (
+    <div className="info-badge inline-flex items-center bg-white p-2 rounded-lg shadow-md animate-float">
+      <PeopleIcon className="text-blue-500" />
+      <div className="ml-2">
+        <p className="text-sm font-bold">100%</p>
+        <p className="text-xs">Satisfied Students</p>
+      </div>
+    </div>
+  );
+};
+
+const InfoBoxWithArrow: React.FC = () => {
+  return (
+    <div className="info-box-with-arrow bg-white p-4 rounded-lg shadow-md relative">
+      <p className="text-sm ">Dear Learner&rsquo;s <br/>Learn everything here from scratch</p>
+      <p className="text-sm font-bold">- Suraj Kumar (Founder)</p>
+      <div className="arrow"></div>
+    </div>
+  );
+};
 
 const Introduction: React.FC = () => {
   return (
-    <div className="introduction-container">
-      <div className="main-image-container">
-        <Image src={topImage} alt="Learning with Basics Top Image" width={1920} height={1080} className="main-image" />
-        <button className="main-image-button">Contact us</button>
-      </div>
-      <div className="side-image-card">
-        <div className="side-image-container">
-          <Image src={SKJImage} alt="Learning with basics Founder Image"  width={960} height={1080} className="side-image" />
-        </div>
-        <p className="side-image-description">
-          Dear Learner&apos;s, Learn everything here with scratch.
-          <br />
-          <br/>
-          &quot;Aao milkar seekhte aur seekhate hain&quot;
-          <br/>
-            <span className="float-end">- Suraj Kumar </span>
+    <div className="mt-3 introduction-container bg-[#d4ddf5] p-4 flex flex-col sm:flex-row items-center justify-evenly rounded-lg">
+      <div className="text-content flex-1 sm:w-3/5 space-y-6">
+        <p className="text-5xl font-bold mb-4">
+          Find Your Perfect Courses & Improve Your Skills
         </p>
+        <p className="text-lg mb-4">
+          Comprehensive video lectures, student success stories and personalized
+          learning experience to help excel in your journey.
+        </p>
+        <div className="button-group flex gap-4 ">
+          <button className="btn-primary">
+            Explore Video Library&nbsp;
+            <ArrowCircleRightIcon />
+          </button>
+          <button className="btn-secondary">
+            Explore Success Stories&nbsp;
+            <ArrowCircleRightIcon />
+          </button>
+        </div>
+      </div>
+      <div
+        className="image-content relative hidden sm:block overflow-hidden sm:w-2/5 place-items-end mb-4 mr-4"
+        style={{ height: "350px" }}
+      >
+        <Image
+          src={SKJImage}
+          height={300}
+          width={300}
+          alt="Learning with basics Founder Image"
+          className="rounded-lg object-cover"
+          style={{ objectPosition: "top" }}
+        />
+        <div className="absolute bottom-0 right-5 m-2 hover:transform hover:scale-105 transition-transform duration-300">
+          <InfoBadge2 />
+        </div>
+        <div className="infobadge-2 top-10 absolute m-2 hover:transform hover:scale-105 transition-transform duration-300">
+          <InfoBadge />
+        </div>
+        <div className="absolute bottom-5 left-0 m-2 hover:transform hover:scale-105 transition-transform duration-300">
+          <InfoBoxWithArrow />
+        </div>
       </div>
     </div>
   );
