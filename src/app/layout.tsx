12ace59ab65e91cb4cbf82a4e741ec0469cd2fec
Harsh raj <harshraj17087@gmail.com>
2025-01-26 00:50:50 +0530

    added SSR for font load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
-import Header from '@/components/Header';
-import type { Metadata } from 'next';
-import './globals.css';
-import Footer from '@/components/Footer';
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import Header from "@/components/Header";
+import type { Metadata } from "next";
+import "./globals.css";
+import Footer from "@/components/Footer";
+import { nunito } from "@/utils/styles/font";
+
 export const metadata: Metadata = {
-  title: 'Learning With Basics',
-  description: 'Learning With Basics',
+  title: "Learning With Basics",
+  description: "Learning With Basics",
 };
 
 export default function RootLayout({
@@ -14,10 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Header />
-        {children}
-        <Footer/>
+      <body className={`${nunito.className}`}>
+        <AppRouterCacheProvider>
+          <Header />
+          {children}
+          <Footer />
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
